6bf91899e712d9db2239bc25a7c40b60d51b3cc9
kentakom1213 <kentakom1213@gmail.com>
2025-05-27 00:30:08 +0900

    add: 微修正

--- src/components/edit_menu.rs
+++ src/components/edit_menu.rs
@@ -4,94 +4,105 @@ use crate::{mode::EditMode, GraphEditorApp};
 
 /// 編集メニューを表示する
 pub fn draw_edit_menu(app: &mut GraphEditorApp, ctx: &Context) {
-    egui::SidePanel::left("Menu").show(ctx, |ui| {
-        // カーソルがあるか判定
-        app.hovered_on_menu_window = ui.rect_contains_pointer(ui.max_rect());
+    egui::SidePanel::left("Menu")
+        .min_width(200.0)
+        .show(ctx, |ui| {
+            // カーソルがあるか判定
+            app.hovered_on_menu_window = ui.rect_contains_pointer(ui.max_rect());
 
-        egui::Frame::new()
-            .inner_margin(egui::Margin::same(10))
-            .show(ui, |ui| {
-                ui.vertical(|ui| {
-                    // モード切替
-                    ui.label(egui::RichText::new("Edit Mode").size(app.config.menu_font_size_mini));
+            egui::Frame::new()
+                .inner_margin(egui::Margin::same(10))
+                .show(ui, |ui| {
+                    ui.vertical(|ui| {
+                        // モード切替
+                        ui.label(
+                            egui::RichText::new("Edit Mode").size(app.config.menu_font_size_mini),
+                        );
 
-                    ui.radio_value(
-                        &mut app.edit_mode,
-                        EditMode::default_normal(),
-                        egui::RichText::new("Normal [Esc]").size(app.config.menu_font_size_normal),
-                    );
-                    ui.radio_value(
-                        &mut app.edit_mode,
-                        EditMode::default_add_vertex(),
-                        egui::RichText::new("Add Vertex [V]")
-                            .size(app.config.menu_font_size_normal),
-                    );
-                    ui.radio_value(
-                        &mut app.edit_mode,
-                        EditMode::default_add_edge(),
-                        egui::RichText::new("Add Edge [E]").size(app.config.menu_font_size_normal),
-                    );
-                    ui.radio_value(
-                        &mut app.edit_mode,
-                        EditMode::default_delete(),
-                        egui::RichText::new("Delete [D]").size(app.config.menu_font_size_normal),
-                    );
+                        ui.radio_value(
+                            &mut app.edit_mode,
+                            EditMode::default_normal(),
+                            egui::RichText::new("Normal [Esc]")
+                                .size(app.config.menu_font_size_normal),
+                        );
+                        ui.radio_value(
+                            &mut app.edit_mode,
+                            EditMode::default_add_vertex(),
+                            egui::RichText::new("Add Vertex [V]")
+                                .size(app.config.menu_font_size_normal),
+                        );
+                        ui.radio_value(
+                            &mut app.edit_mode,
+                            EditMode::default_add_edge(),
+                            egui::RichText::new("Add Edge [E]")
+                                .size(app.config.menu_font_size_normal),
+                        );
+                        ui.radio_value(
+                            &mut app.edit_mode,
+                            EditMode::default_delete(),
+                            egui::RichText::new("Delete [D]")
+                                .size(app.config.menu_font_size_normal),
+                        );
 
-                    ui.separator();
+                        ui.separator();
 
-                    // 0-indexed / 1-indexed の選択
-                    ui.label(
-                        egui::RichText::new("Indexing [1]").size(app.config.menu_font_size_mini),
-                    );
+                        // 0-indexed / 1-indexed の選択
+                        ui.label(
+                            egui::RichText::new("Indexing [1]")
+                                .size(app.config.menu_font_size_mini),
+                        );
 
-                    ui.radio_value(
-                        &mut app.zero_indexed,
-                        true,
-                        egui::RichText::new("0-indexed").size(app.config.menu_font_size_normal),
-                    );
-                    ui.radio_value(
-                        &mut app.zero_indexed,
-                        false,
-                        egui::RichText::new("1-indexed").size(app.config.menu_font_size_normal),
-                    );
+                        ui.radio_value(
+                            &mut app.zero_indexed,
+                            true,
+                            egui::RichText::new("0-indexed").size(app.config.menu_font_size_normal),
+                        );
+                        ui.radio_value(
+                            &mut app.zero_indexed,
+                            false,
+                            egui::RichText::new("1-indexed").size(app.config.menu_font_size_normal),
+                        );
 
-                    ui.separator();
+                        ui.separator();
 
-                    ui.label(
-                        egui::RichText::new("Direction [Shift + D]")
-                            .size(app.config.menu_font_size_mini),
-                    );
-                    ui.radio_value(
-                        &mut app.graph.is_directed,
-                        false,
-                        egui::RichText::new("Undirected").size(app.config.menu_font_size_normal),
-                    );
-                    ui.radio_value(
-                        &mut app.graph.is_directed,
-                        true,
-                        egui::RichText::new("Directed").size(app.config.menu_font_size_normal),
-                    );
+                        ui.label(
+                            egui::RichText::new("Direction [Shift + D]")
+                                .size(app.config.menu_font_size_mini),
+                        );
+                        ui.radio_value(
+                            &mut app.graph.is_directed,
+                            false,
+                            egui::RichText::new("Undirected")
+                                .size(app.config.menu_font_size_normal),
+                        );
+                        ui.radio_value(
+                            &mut app.graph.is_directed,
+                            true,
+                            egui::RichText::new("Directed").size(app.config.menu_font_size_normal),
+                        );
 
-                    ui.separator();
+                        ui.separator();
 
-                    ui.checkbox(
-                        &mut app.is_animated,
-                        egui::RichText::new("Animate [A]").size(app.config.menu_font_size_normal),
-                    );
+                        ui.checkbox(
+                            &mut app.is_animated,
+                            egui::RichText::new("Animate [A]")
+                                .size(app.config.menu_font_size_normal),
+                        );
 
-                    ui.separator();
+                        ui.separator();
 
-                    // グラフのクリア
-                    if ui
-                        .button(
-                            egui::RichText::new("Clear All").size(app.config.menu_font_size_normal),
-                        )
-                        .clicked()
-                    {
-                        app.graph.clear();
-                        app.next_z_index = 0;
-                    }
+                        // グラフのクリア
+                        if ui
+                            .button(
+                                egui::RichText::new("Clear All")
+                                    .size(app.config.menu_font_size_normal),
+                            )
+                            .clicked()
+                        {
+                            app.graph.clear();
+                            app.next_z_index = 0;
+                        }
+                    });
                 });
-            });
-    });
+        });
 }
--- src/components/graph_io.rs
+++ src/components/graph_io.rs
@@ -11,6 +11,7 @@ pub fn draw_graph_io(app: &mut GraphEditorApp, ctx: &Context) {
     // テキストの表示
     egui::Window::new("Graph Input")
         .collapsible(false)
+        .default_width(150.0)
         .show(ctx, |ui| {
             // カーソルがあるか判定
             app.hovered_on_input_window = ui.rect_contains_pointer(ui.max_rect());
